01078677e0f7690d5dfb257fd9c919ab46753731
LA4AM12 <xingxiu.fyc@gmail.com>
2023-02-23 20:22:15 +0800

    random

---
+++ src/la4am12/minmin/MinMinScheduler.java
@@ -0,0 +1,75 @@
+package la4am12.minmin;
+
+import la4am12.Scheduler;
+import org.cloudbus.cloudsim.Cloudlet;
+import org.cloudbus.cloudsim.Vm;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * @author : LA4AM12
+ * @create : 2023-02-23 16:47:26
+ * @description : MinMin + Greedy Algorithm
+ */
+public class MinMinScheduler extends Scheduler {
+	public MinMinScheduler(List<Cloudlet> cloudletList, List<Vm> vmList) {
+		super(cloudletList, vmList);
+		Collections.sort(cloudletList, Comparator.comparingLong(Cloudlet::getCloudletLength));
+		Collections.sort(vmList, Comparator.comparingDouble(Vm::getMips));
+	}
+
+	@Override
+	public int[] allocate() {
+		int[] cloudletToVm = new int[cloudletNum];
+
+
+		// time[i][j] denotes the execution time of task i on virtual machine j
+		double[][] time = new double[cloudletNum][vmNum];
+		for (int i = 0; i < cloudletNum; i++) {
+			for (int j = 0; j < vmNum; j++) {
+				time[i][j] = cloudletList.get(i).getCloudletLength() / vmList.get(j).getMips();
+			}
+		}
+
+		// vms' uptime
+		double[] vmUptime = new double[vmNum];
+		// the cloudlet num running on specific vm
+		int[] vmTaskCount = new int[vmNum];
+
+
+		// Prioritise mapping of small tasks to faster executing machines
+		vmUptime[vmNum - 1] = time[0][vmNum - 1];
+		vmTaskCount[vmNum - 1]++;
+		cloudletToVm[0] = vmList.get(vmNum - 1).getId();
+
+		int idx;
+		double timeSpan;
+		for (int i = 1; i < cloudletNum; i++) {
+			timeSpan = vmUptime[vmNum - 1] + time[i][vmNum - 1];
+			idx = vmNum - 1;
+
+			for (int j = vmNum - 2; j >= 0; j--) {
+				if (vmUptime[j] == 0) {
+					if (timeSpan >= time[i][j]) {
+						idx = j;
+					}
+					break;
+				}
+
+				if (timeSpan > vmUptime[j] + time[i][j]) {
+					timeSpan = vmUptime[j] + time[i][j];
+					idx = j;
+				} else if (timeSpan == vmUptime[j] + time[i][j] && vmTaskCount[j] < vmTaskCount[idx]) {
+					idx = j;
+				}
+			}
+			vmUptime[idx] += time[i][idx];
+			vmTaskCount[idx]++;
+			cloudletToVm[i] = vmList.get(idx).getId();
+		}
+
+		return cloudletToVm;
+	}
+}
---
+++ src/la4am12/random/RandomScheduler.java
@@ -0,0 +1,29 @@
+package la4am12.random;
+
+import la4am12.Scheduler;
+import org.cloudbus.cloudsim.Cloudlet;
+import org.cloudbus.cloudsim.Vm;
+
+import java.util.List;
+import java.util.Random;
+
+/**
+ * @author : LA4AM12
+ * @create : 2023-02-23 16:17:50
+ * @description : Random schedule
+ */
+public class RandomScheduler extends Scheduler {
+	public RandomScheduler(List<Cloudlet> cloudletList, List<Vm> vmList) {
+		super(cloudletList, vmList);
+	}
+
+	@Override
+	public int[] allocate() {
+		int[] cloudletToVm = new int[cloudletNum];
+		Random random = new Random();
+		for (int i = 0; i < cloudletNum; i++) {
+			cloudletToVm[i] = random.nextInt(vmNum);
+		}
+		return cloudletToVm;
+	}
+}
